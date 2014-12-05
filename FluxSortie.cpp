d3cd5f61fe4a4b232a8f9522356779df9ef13ac8
Sylvie Stucki <sylvie.stucki@a3.epfl.ch>
2014-12-05 17:48:12 +0100

    Added the licence disclaimer to FluxSortie.h and JournalTemporaire.h and fixed some behaviours.

--- FluxSortie.cpp
+++ FluxSortie.cpp
@@ -1,8 +1,35 @@
+/*************************************************************************
+ * Copyright (©) 2011-2014 EPFL (Ecole Polytechnique fédérale de Lausanne)
+ * Laboratory of Geographic information systems (LaSIG)
+ * 
+ * This file is part of Sambada.
+ *  
+ * Sambada is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 3 of the License, or (at your option) any later version.
+ * Sambada is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Sambada ; if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Authors : Sylvie Stucki (sylvie.stucki@a3.epfl.ch), Stéphane Joost (stephane.joost@epfl.ch) 
+ * Laboratory of Geographic information systems
+ * EPFL ENAC IIE LASIG
+ * Station 18
+ * CH-1015 Lausanne
+ * Web site : http://lasig.epfl.ch/sambada
+ * 
+ * Sambada includes two libraries: Scythe Statistical Library (under GPL 3) and Shapefile C Library (under LGPL 2.1, courtesy of Frank Warmerdam).
+ * 
+ * Scythe Statistical Library
+ * Copyright (C) 2000-2002 Andrew D. Martin and Kevin M. Quinn;
+ * 2002-2012 Andrew D. Martin, Kevin M. Quinn, and Daniel Pemstein.  All Rights Reserved.
+ * 
+ * Shapefile C Library
+ * Copyright (c) 1999, Frank Warmerdam
+ *************************************************************************/
+
 #include "fluxsortie.h"
 #include <iomanip>
 
 FluxSortie::FluxSortie()
-    :delimLignes("\n"), delimMots(" "), terminalActif(true), fichierActif(true), fichierOperationnel(false), estMessageErreur(false)
+:delimLignes("\n"), delimMots(" "), terminalActif(true), fichierActif(true), fichierOperationnel(false), estMessageErreur(false)
 {
 }
 
@@ -75,19 +102,27 @@ void FluxSortie::setActivites(bool term, bool fichier)
 
 bool FluxSortie::ouvertureFichier()
 {
-    if( nomFichier.size()==0 )
-    {
-        return false;
-    }
+	if (testeValiditeFichier())
+	{
+		return true;
+	}
+	
+	if( nomFichier.size()==0 )
+	{
+		return false;
+	}
 
-    sortie.open(nomFichier.c_str());
-    if (sortie.fail())
-    {
-        return false;
-    }
-
-    fichierOperationnel=true;
-    return true;
+	sortie.close();
+	
+	sortie.open(nomFichier.c_str());
+	if (sortie.fail())
+	{
+		return false;
+	}
+	
+	fichierOperationnel=true;
+	return true;
+	
 }
 
 void FluxSortie::fermetureFichier()
@@ -105,8 +140,8 @@ bool FluxSortie::testeValiditeFichier()
 	{
 		fichierOperationnel=true;
 	}
-
-
+	
+	
     return fichierOperationnel;
 }
 
@@ -139,7 +174,7 @@ FluxSortie& FluxSortie::operator<<(ostream &(*pf)(ostream &))
     {
         pf(cout);
     }
-
+	
     if (fichierActif && fichierOperationnel)
     {
         pf(sortie);
@@ -178,4 +213,8 @@ FluxSortie& erreur (FluxSortie& fs)
     fs.erreurDetectee();
     return fs;
 }
+
+FluxSortie::FluxSortie(const FluxSortie& fs)
+{}
+
 
