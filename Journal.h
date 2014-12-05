4293b5e77e8682c5285204cc095fe3205444ebc1
Sylvie Stucki <sylvie.stucki@a3.epfl.ch>
2014-12-05 17:49:41 +0100

    Added a class Journal to handle log files. It has both a FluxSortie and a JournalTemporaire to manage logging during Sambada's initialisation.

---
+++ Journal.h
@@ -0,0 +1,93 @@
+/*************************************************************************
+ * Copyright (©) 2011-2014 EPFL (Ecole Polytechnique fédérale de Lausanne)
+ * Laboratory of Geographic information systems (LaSIG)
+ * 
+ * This file is part of Sambada.
+ *  
+ * Sambada is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 3 of the License, or (at your option) any later version.
+ * Sambada is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Sambada ; if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Authors : Sylvie Stucki (sylvie.stucki@a3.epfl.ch), Stéphane Joost (stephane.joost@epfl.ch) 
+ * Laboratory of Geographic information systems
+ * EPFL ENAC IIE LASIG
+ * Station 18
+ * CH-1015 Lausanne
+ * Web site : http://lasig.epfl.ch/sambada
+ * 
+ * Sambada includes two libraries: Scythe Statistical Library (under GPL 3) and Shapefile C Library (under LGPL 2.1, courtesy of Frank Warmerdam).
+ * 
+ * Scythe Statistical Library
+ * Copyright (C) 2000-2002 Andrew D. Martin and Kevin M. Quinn;
+ * 2002-2012 Andrew D. Martin, Kevin M. Quinn, and Daniel Pemstein.  All Rights Reserved.
+ * 
+ * Shapefile C Library
+ * Copyright (c) 1999, Frank Warmerdam
+ *************************************************************************/
+
+#ifndef JOURNAL_H
+#define JOURNAL_H
+
+#include "FluxSortie.h"
+
+using namespace std;
+
+class Journal
+{
+public:
+	Journal();
+	virtual ~Journal();
+	
+	void metFlotEnPause();
+	bool sortDeLaPause();
+	
+	void setDelimLignes(const string& delim);
+    string getDelimLignes() const;
+	
+    void setDelimMots(const string& delim);
+    string getDelimMots() const;
+	
+    void setNomFichier(const string& nom);
+    string getNomFichier() const;
+	
+    void setDelims(const string& delimL, const string& delimM);
+	
+    void setActiviteTerminal(bool b);
+    bool getActiviteTerminal() const;
+	
+    void setActiviteFichier(bool b);
+    bool getActiviteFichier() const;
+	
+    void setActivites(bool term, bool fichier);
+			
+    void erreurDetectee();
+	
+    template<class T>
+    Journal& ecrit(const T& token);
+	
+    Journal& retourLigne();
+	
+    Journal& nouvMot();
+	
+    template<class T>
+    Journal& operator<<(const T& token);
+	
+    Journal& operator<<(Journal& (*pf)(Journal&));
+	
+    Journal& operator<<(ostream& (*pf)(ostream&));
+	
+	
+protected:
+	
+	FluxSortie flux;
+	JournalTemporaire temp;
+	
+	bool flotEnPause;
+	
+
+private: 
+	Journal(const Journal& j);
+	
+};
+
+#endif // JOURNAL_H
