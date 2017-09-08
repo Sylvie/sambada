1c5a6085f6a2427b640066838366022364eb6120
Sylvie <Sylvie@users.noreply.github.com>
2017-09-08 18:18:37 +0200

    SMB-18: Placement des programmes dans un dossier "binaries"
    Les quatres programmes sont placés dans le dossier "$(builddir)/binaries" afin de faciliter leur emballage.

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -1,6 +1,6 @@
-bin_PROGRAMS = sambada supervision recode-plink recode-plink-lfmm
+bin_PROGRAMS = binaries/sambada binaries/supervision binaries/recode-plink binaries/recode-plink-lfmm
 
-sambada_SOURCES = src/mainSambada.cpp \
+binaries_sambada_SOURCES = src/mainSambada.cpp \
 	src/RegressionLogistique-io.cpp \
 	src/RegressionLogistique-calc.cpp \
 	src/RegressionLogistique.h \
@@ -18,13 +18,13 @@ sambada_SOURCES = src/mainSambada.cpp \
 	src/Chronometre.h \
 	src/Duree.cpp \
 	src/Duree.h
-sambada_CPPFLAGS = -I $(srcdir)/ext/scythestat-1.0.3/scythestat \
--I $(srcdir)/ext/scythestat-1.0.3/scythestat/rng \
--I $(srcdir)/ext/shapelib-1.3.0
-sambada_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
-sambada_LDADD = libshp.a libintermediate.a
+binaries_sambada_CPPFLAGS = -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
+	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng \
+	-I $(top_srcdir)/ext/shapelib-1.3.0
+binaries_sambada_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
+binaries_sambada_LDADD = libshp.a libintermediate.a
 
-supervision_SOURCES = src/mainSupervision.cpp \
+binaries_supervision_SOURCES = src/mainSupervision.cpp \
 	src/Supervision.cpp \
 	src/Supervision.h \
 	src/RegressionLogistique.h \
@@ -32,28 +32,28 @@ supervision_SOURCES = src/mainSupervision.cpp \
 	src/Archiviste.cpp \
 	src/Archiviste.h \
 	src/Erreur.h
-supervision_CPPFLAGS = -I $(srcdir)/ext/scythestat-1.0.3/scythestat \
-	-I $(srcdir)/ext/scythestat-1.0.3/scythestat/rng
-supervision_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
-supervision_LDADD = libintermediate.a
+binaries_supervision_CPPFLAGS = -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
+	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng
+binaries_supervision_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
+binaries_supervision_LDADD = libintermediate.a
 
-recode_plink_SOURCES = src/recodePlink.cpp \
+binaries_recode_plink_SOURCES = src/recodePlink.cpp \
 	src/RegressionLogistique.h \
 	src/Toolbox.h \
 	src/Erreur.h
-recode_plink_CPPFLAGS = -I $(srcdir)/ext/scythestat-1.0.3/scythestat \
-	-I $(srcdir)/ext/scythestat-1.0.3/scythestat/rng
-recode_plink_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
-recode_plink_LDADD = libintermediate.a
+binaries_recode_plink_CPPFLAGS = -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
+	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng
+binaries_recode_plink_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
+binaries_recode_plink_LDADD = libintermediate.a
 
-recode_plink_lfmm_SOURCES = src/recodeLFMM.cpp \
+binaries_recode_plink_lfmm_SOURCES = src/recodeLFMM.cpp \
 	src/RegressionLogistique.h \
 	src/Toolbox.h \
 	src/Erreur.h
-recode_plink_lfmm_CPPFLAGS = -I $(srcdir)/ext/scythestat-1.0.3/scythestat \
-	-I $(srcdir)/ext/scythestat-1.0.3/scythestat/rng
-recode_plink_lfmm_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
-recode_plink_lfmm_LDADD = libintermediate.a
+binaries_recode_plink_lfmm_CPPFLAGS = -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
+	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng
+binaries_recode_plink_lfmm_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
+binaries_recode_plink_lfmm_LDADD = libintermediate.a
 
 noinst_LIBRARIES = libshp.a libintermediate.a
 
@@ -63,8 +63,8 @@ libintermediate_a_SOURCES = src/RegressionLogistique-bienvenue.cpp \
 	src/Toolbox.h \
 	src/Erreur.cpp \
 	src/Erreur.h
-libintermediate_a_CPPFLAGS = -I $(srcdir)/ext/scythestat-1.0.3/scythestat \
-	-I $(srcdir)/ext/scythestat-1.0.3/scythestat/rng
+libintermediate_a_CPPFLAGS = -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
+	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng
 libintermediate_a_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
 
 libshp_a_SOURCES = ext/shapelib-1.3.0/shpopen.c \
