37e7008081b8b8705c46352b486b39a3fff4b682
Sylvie <Sylvie@users.noreply.github.com>
2017-09-29 21:59:14 +0200

    SMB-18: Ajout des exemples dans les fichiers distribués
    Les fichiers d'exemples sont listés par la variable dist_pkgdata_DATA
    dans le fichier examples/Makefile.mk
    Ce fichier est inclus dans Makefile.am

---
+++ examples/Makefile.mk
@@ -0,0 +1,22 @@
+dist_pkgdata_DATA += \
+	examples/DataFromManual \
+	examples/DataFromManual/OneDataFile \
+	examples/DataFromManual/OneDataFile/combo-data.txt \
+	examples/DataFromManual/OneDataFile/param-combo-a.txt \
+	examples/DataFromManual/OneDataFile/param-combo-b.txt \
+	examples/DataFromManual/OneDataFile/param-combo.txt \
+	examples/DataFromManual/OneDataFile/param-split-combo.txt \
+	examples/DataFromManual/TwoDataFiles \
+	examples/DataFromManual/TwoDataFiles/env-data.txt \
+	examples/DataFromManual/TwoDataFiles/mol-data.txt \
+	examples/DataFromManual/TwoDataFiles/param-a.txt \
+	examples/DataFromManual/TwoDataFiles/param-b.txt \
+	examples/DataFromManual/TwoDataFiles/param-split.txt \
+	examples/DataFromManual/TwoDataFiles/param.txt \
+	examples/RandomData \
+	examples/RandomData/input-Sample100.txt \
+	examples/RandomData/Sample100.txt \
+	examples/SubsetCattleSNP \
+	examples/SubsetCattleSNP/extrait_marq.txt \
+	examples/SubsetCattleSNP/paramTest.txt \
+	examples/SubsetCattleSNP/TableEnvUG.csv
