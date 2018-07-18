b3e66f86d6ab732b65536af6bd5d6a9c9e523ed9
Sylvie <Sylvie@users.noreply.github.com>
2018-07-18 08:06:35 +0200

    SMB-28: Squelette de tests d'intégration pour Sambada (2)
    Ajout de catch.hpp dans la liste des sources de la bibliothèque
    intermédiaire libSambadaTestUtils.a, sinon ce fichier n'est pas ajouté
    dans la distribution.

--- test/utils/Makefile.mk
+++ test/utils/Makefile.mk
@@ -4,8 +4,9 @@ catch_source_files = \
 
 noinst_LIBRARIES += test/utils/libSambadaTestUtils.a
 
-test_utils_libSambadaTestUtils_a_SOURCES = \
+test_utils_libSambadaTestUtils_a_SOURCES = 	\
     test/utils/SambadaRegressionResults.cpp \
-    test/utils/SambadaRegressionResults.h
+    test/utils/SambadaRegressionResults.h 	\
+    test/utils/catch.hpp
 test_utils_libSambadaTestUtils_a_CPPFLAGS = -I $(top_srcdir)/test/utils/
 
