c667e0ae19bca225014f445f0bd134a4649545e7
Sylvie <Sylvie@users.noreply.github.com>
2018-07-30 21:34:15 +0200

    SMB-28: Squelette de tests d'intégration pour Sambada (8)
    Correction d'une coquille dans test/integration/sambada/Makefile.mk.

--- test/integration/sambada/Makefile.mk
+++ test/integration/sambada/Makefile.mk
@@ -12,7 +12,7 @@ test_integration_SambadaIntegrationTests_LDADD = \
 		$(top_builddir)/test/integration/utils/libSambadaIntegrationTestUtils.a \
 		$(top_builddir)/test/utils/libSambadaTestUtils.a
 
-include test/integration/sambada/regressionresultsIntTests/Makefile.mk
+include test/integration/sambada/regressionResultsIntTests/Makefile.mk
 
 ## Cleaning ##
 clean-local-test-integration-sambada:
