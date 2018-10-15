3db88233c9807866fab8b4aa3023cd141246db50
Sylvie <Sylvie@users.noreply.github.com>
2018-10-15 18:51:32 +0200

    SMB-17: Ajout de tests pour les fichiers préparés par Supervision (5)
    Sambada est capable d'analyser les fichiers créés par Supervision
    si le fichier de marqueurs n'a pas d'extention alors que le paramètre
    OUTPUTFILE est présent (avec ou sans extension).
    
    (!) Attention: Le test échoue lamentablement s'il n'y a pas de
    paramètre OUTPUTFILE.

--- test/integration/sambada/splitInputFilesIntTests/Makefile.mk
+++ test/integration/sambada/splitInputFilesIntTests/Makefile.mk
@@ -1,12 +1,15 @@
 
 test_integration_SambadaIntegrationTests_SOURCES += \
-		test/integration/sambada/splitInputFilesIntTests/splitInputFilesWithFilenamesExtensionsIntTests.cpp
+		test/integration/sambada/splitInputFilesIntTests/splitInputFilesWithFilenamesExtensionsIntTests.cpp \
+		test/integration/sambada/splitInputFilesIntTests/splitInputFilesWithoutFilenamesExtensionsIntTests.cpp
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-env-cattle.csv \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-mark-cattle.txt \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-mark-cattle-mark-0-0.txt \
+		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-mark-cattle-mark-0-0 \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-mark-cattle-mark-1-3.txt \
+		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/choice-mark-cattle-mark-1-3 \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/param-0-0-no-outputfile.txt \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/param-0-0-outputfile-no-ext.txt \
 		$(top_srcdir)/test/integration/sambada/splitInputFilesIntTests/param-0-0-outputfile-with-ext.txt \
