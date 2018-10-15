42d7eeb7e217fd4aeaca7568665483e12472fd48
Sylvie <Sylvie@users.noreply.github.com>
2018-10-15 20:38:35 +0200

    SMB-17: Ajout de tests pour les fichiers de données combinés
    Les tests écrits jusqu'ici comprenaient des jeux de données séparés en
    données environnementales et moléculaires.
    Or certaines fonctionnalités doivent aussi être testés pour les jeux de
    données combinés.
    
    Ajout de tests pour les analyses pour lesquelles les résultats se
    trouvent dans le même dossier que le jeu de données combiné (= sans le
    param OUTPUTFILE) et aussi pour le cas où les résultats se trouvent dans
    un autre dossier (= avec le param OUTPUTFILE).

---
+++ test/integration/sambada/resultFilesLocationIntTests/resultsInDifferentFolderAsUniqueDataFileIntTest.cpp
@@ -0,0 +1,92 @@
+#include "catch.hpp"
+#include "SambadaRegressionResults.h"
+#include "SambadaIntegrationTestUtils.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
+
+SCENARIO("Test that result files are writen in a different folder than the unique data file", "[results-different-folder-unique-data-int]") {
+
+    INFO("Working folder: " + SambadaIntegrationTestUtils::runCommand("pwd"));
+
+    GIVEN("A program to run, its expected output file, and the file of expected results")
+    {
+        std::string program(SambadaIntegrationTestUtils::computePlatformSpecificProgramName("./binaries/sambada"));
+
+        std::string pathToOutputFolder("./test/integration/sambada/resultFilesLocationIntTests/resultsInDifferentFolderAsUniqueDataFile/");
+        fs::create_directory(pathToOutputFolder.c_str());
+
+        std::string fileNameOut0(pathToOutputFolder + "choice-data-cattle-Out-0.txt");
+        std::string fileNameOut1(pathToOutputFolder + "choice-data-cattle-Out-1.txt");
+        std::string fileNameLogs(pathToOutputFolder + "choice-data-cattle-log.txt");
+
+        std::string pathToInputFolder(SambadaIntegrationTestUtils::getTopSourceDirectory() + "test/integration/sambada/resultFilesLocationIntTests/");
+        std::string fileNameParam(pathToInputFolder + "param-with-outputfile-setting-unique-data-file.txt");
+        std::string fileNameData(pathToInputFolder + "choice-data-cattle.csv");
+
+        std::string fileNameExpectedResultsDim0(pathToInputFolder + "expected-results-cattle-dim-0.txt");
+        std::string fileNameExpectedResultsDim1(pathToInputFolder + "expected-results-cattle-dim-1.txt");
+
+        std::ifstream lecteurCorrige(fileNameExpectedResultsDim0.c_str());
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedNullResults(SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 0));
+        lecteurCorrige.close();
+        expectedNullResults.verifieTailles(true, 0, 5);
+
+        lecteurCorrige.open(fileNameExpectedResultsDim1.c_str());
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedResults(SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 1));
+        lecteurCorrige.close();
+        expectedResults.verifieTailles(true, 1, 30);
+
+        WHEN("the program is run")
+        {
+            std::string output = SambadaIntegrationTestUtils::runCommand(program + " " + fileNameParam + " " + fileNameData);
+            INFO(output);
+
+            THEN("the output files are found")
+            {
+                std::ifstream lecteurOut0(fileNameOut0.c_str());
+                REQUIRE(lecteurOut0.good());
+                REQUIRE(lecteurOut0.is_open());
+
+                std::ifstream lecteurOut1(fileNameOut1.c_str());
+                REQUIRE(lecteurOut1.good());
+                REQUIRE(lecteurOut1.is_open());
+
+                AND_WHEN("the output files are read")
+                {
+                    SambadaRegressionResults resultsDim0(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut0, true, 0));
+                    SambadaRegressionResults resultsDim1(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut1, true, 1));
+
+                    THEN("the results match the expectations")
+                    {
+                        resultsDim0.verifieTailles(true, 0, 5);
+                        resultsDim0.compare(expectedNullResults);
+
+                        resultsDim1.verifieTailles(true, 1, 30);
+                        resultsDim1.compare(expectedResults);
+                    }
+                }
+
+                lecteurOut0.close();
+                lecteurOut1.close();
+            }
+
+            std::remove(fileNameOut0.c_str());
+            std::remove(fileNameOut1.c_str());
+            std::remove(fileNameLogs.c_str());
+
+            std::remove(pathToOutputFolder.c_str());
+        }
+    }
+}
+
