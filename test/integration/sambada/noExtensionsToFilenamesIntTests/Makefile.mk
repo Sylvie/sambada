a406b269668780b8fba9ba04be5192ad58e5ab86
Sylvie <Sylvie@users.noreply.github.com>
2018-10-25 09:06:40 +0200

    SMB-17: Ajout de tests avec un seul fichier de données (2)
    Ajout des nouveaux fichiers dans la liste EXTRA_DIST du Makefile.am

--- test/integration/sambada/noExtensionsToFilenamesIntTests/Makefile.mk
+++ test/integration/sambada/noExtensionsToFilenamesIntTests/Makefile.mk
@@ -4,12 +4,21 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/noExtensionsToFilenamesIntTests/noExtensionsToFilenamesUniqueFileIntTests.cpp
 
 EXTRA_DIST += \
+		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-data-cattle.csv \
+		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-data-cattle \
 		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-env-cattle.csv \
 		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-env-cattle \
 		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-mark-cattle.txt \
 		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/choice-mark-cattle \
-		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param.txt \
-		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param \
-		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file-no-ext.txt \
+		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file-no-ext.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file-with-prefix.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-marker-file-with-prefix-no-ext.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-unique-data-file \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-unique-data-file.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-unique-data-file-no-ext.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-unique-data-file-with-prefix.txt \
+        $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/param-unique-data-file-with-prefix-no-ext.txt \
 		$(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/expected-results-cattle-dim-0.txt \
         $(top_srcdir)/test/integration/sambada/noExtensionsToFilenamesIntTests/expected-results-cattle-dim-1.txt
