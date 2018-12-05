85428346148b5c701acb2b4f19b92505d007d47e
Sylvie <Sylvie@users.noreply.github.com>
2018-12-05 21:35:15 +0100

    SMB-51: Configuration de la mesure de couverture de tests
    Ajout d'un appel à gcov (pour la classe Archiviste) et ajout du chemin
    vers le dossier build dans le script de récolte des résultats.

--- scripts-ci/travis-after-script.sh
+++ scripts-ci/travis-after-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -v
 if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
-    ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+    ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT -p "/home/travis/build/Sylvie/sambada/build/"
 fi
--- scripts-ci/travis-gcov.sh
+++ scripts-ci/travis-gcov.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -v
 echo "TODO: measuring code coverage"
+${GCOV} ../src/Archiviste.cpp src/binaries_sambada-Archiviste.o
