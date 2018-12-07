89fd9c27c4dd0dd22784107759365195ff816219
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 18:09:41 +0100

    SMB-51: Appel à "before-build" dans le dossier "build"
    Apparemment, la commande "before-build" doit être appelée dans le
    même dossier que les tests et "after-build".

--- scripts-ci/travis-before-script.sh
+++ scripts-ci/travis-before-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -v
+mkdir -p build
 if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
     curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
     chmod +x ./cc-test-reporter
-    ./cc-test-reporter before-build
+    cd build
+    ../cc-test-reporter before-build
 fi
