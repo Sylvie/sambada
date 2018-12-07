5550345190b9de431d2ccc6acf282cbcf4c0c9f7
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 18:32:37 +0100

    SMB-51: Correction de la commande "format-coverage"
    Elle ne prend pas l'option --exit-code
    (qui est réservé à la commande "after-build").

--- scripts-ci/travis-after-script.sh
+++ scripts-ci/travis-after-script.sh
@@ -4,10 +4,7 @@ if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
     pwd
     cd build
     ls -l
-    ../cc-test-reporter format-coverage \
-        --exit-code $TRAVIS_TEST_RESULT \
-        -t gcov \
-        -d
+    ../cc-test-reporter format-coverage -t gcov -d
     cat coverage/codeclimate.json
     ../cc-test-reporter upload-coverage -d
 fi
