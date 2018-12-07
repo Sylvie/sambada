0b7960479ad3b524acbfb4e4f29faa6754a827d2
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 19:55:46 +0100

    SMB-51: Essai sans l'option "-t gcov" pour la commande "format-coverage"
    Utilisation des mêmes commandes que dans l'exemple.

--- scripts-ci/travis-after-script.sh
+++ scripts-ci/travis-after-script.sh
@@ -4,9 +4,7 @@ if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
     pwd
     cd build
     ls -l
-    ./cc-test-reporter help format-coverage
-    ./cc-test-reporter format-coverage -t gcov -d
+    ./cc-test-reporter format-coverage -d
     cat coverage/codeclimate.json
-    ./cc-test-reporter help upload-coverage
     ./cc-test-reporter upload-coverage -d
 fi
