c87673af0b5613067e7fbbfecd86a58699ef34f6
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 20:35:57 +0100

    SMB-51: Ajout de l'option "-add-prefix" à la commande "format-coverage"
    Le préfixe est nécessaire pour que Code Climate puisse formater les
    résultats en ligne.

--- scripts-ci/travis-after-script.sh
+++ scripts-ci/travis-after-script.sh
@@ -4,7 +4,8 @@ if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
     pwd
     cd build
     ls -l
-    ./cc-test-reporter format-coverage -d
+    ./cc-test-reporter format-coverage -d \
+        --add-prefix "/home/travis/build/Sylvie/sambada/build/"
     cat coverage/codeclimate.json
     ./cc-test-reporter upload-coverage -d
 fi
