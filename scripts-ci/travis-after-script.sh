5594e88ae3b50419f65e042c69212ae8d1730fa1
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 19:25:43 +0100

    SMB-51: Déplacement du test reporter dans le dossier "build"
    Voyons ce que ça donne.

--- scripts-ci/travis-after-script.sh
+++ scripts-ci/travis-after-script.sh
@@ -4,7 +4,9 @@ if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
     pwd
     cd build
     ls -l
-    ../cc-test-reporter format-coverage -t gcov -d
+    ./cc-test-reporter help format-coverage
+    ./cc-test-reporter format-coverage -t gcov -d
     cat coverage/codeclimate.json
-    ../cc-test-reporter upload-coverage -d
+    ./cc-test-reporter help upload-coverage
+    ./cc-test-reporter upload-coverage -d
 fi
--- scripts-ci/travis-before-script.sh
+++ scripts-ci/travis-before-script.sh
@@ -2,8 +2,8 @@
 set -v
 mkdir -p build
 if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
-    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-    chmod +x ./cc-test-reporter
+    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > build/cc-test-reporter
+    chmod +x build/cc-test-reporter
     cd build
-    ../cc-test-reporter before-build
+    ./cc-test-reporter before-build
 fi
