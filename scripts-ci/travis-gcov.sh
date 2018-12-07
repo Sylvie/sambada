c71619417cb0592b4128ac7faf2281e31948d0ea
Sylvie <Sylvie@users.noreply.github.com>
2018-12-07 22:10:20 +0100

    SMB-51: Utilisation de chemins absolus pour gcov
    ... aussi pour le chemin vers le fichier source.

--- scripts-ci/travis-gcov.sh
+++ scripts-ci/travis-gcov.sh
@@ -6,4 +6,4 @@ whoami
 ls -l ../src
 ls -l src/
 echo ${GCOV}
-${GCOV} -r ${TRAVIS_BUILD_DIR}/src/Archiviste.cpp -o src/binaries_sambada-Archiviste.o
+${GCOV} -r ${TRAVIS_BUILD_DIR}/src/Archiviste.cpp -o ${TRAVIS_BUILD_DIR}/build/src/binaries_sambada-Archiviste.o
