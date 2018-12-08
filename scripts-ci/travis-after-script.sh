089b3df68068578c63e0d826438d1fa417d92653
Sylvie <Sylvie@users.noreply.github.com>
2018-12-08 18:08:55 +0100

    SMB-51: Retrait de la configuration de Code Climate
    Comme la configuration Sonar semble fonctionner, retirons les étapes
    "before-script" et "after-script" qui étaient demandées par Code
    Climate.

--- scripts-ci/travis-after-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -v
-if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
-    pwd
-    ls -l
-    ./cc-test-reporter format-coverage -d
-    cat coverage/codeclimate.json
-    ./cc-test-reporter upload-coverage -d
-fi
--- scripts-ci/travis-before-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -v
-if [ "${SAMBADA_BUILD_TYPE}" = "CODE_COVERAGE" ]; then
-    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-    chmod +x ./cc-test-reporter
-    ./cc-test-reporter before-build
-fi
