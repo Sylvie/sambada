6cce465fc0abe998b41ba28bf179d959d6b04993
Sylvie <Sylvie@users.noreply.github.com>
2018-12-11 19:20:34 +0100

    SMB-53: Ré-ordonnage de la compilation sur Travis
    L'ordre utilisé précédemment nécessitait de compiler le projet trois
    fois, car "make binary-archive" était appelé après "make distcheck".
    
    Le nouvel ordre ressemble à celui d'AppVeyor.

--- scripts-ci/travis-build.sh
+++ scripts-ci/travis-build.sh
@@ -19,13 +19,10 @@ make check
 cp test-suite.log ../test-suite-results.log
 if [ "${SAMBADA_BUILD_TYPE}" = "PACKAGE" ]; then
     binaries/sambada ../Data/Example_parameters_input_matrix.txt ../Data/input_matrix.csv
+    make binary-archive
     ls -R
     make clean
     make distcheck sambadahostsystemname=${SAMBADA_OS_NAME}
-    ls -R
-    make binary-archive
-    cd ..
-    ls -R
 else
     pwd
     source ${TRAVIS_BUILD_DIR}/scripts-ci/travis-gcov.sh
