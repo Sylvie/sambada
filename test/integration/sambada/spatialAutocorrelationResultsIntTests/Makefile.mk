d081cf4cdcfff8ab9a0d0e9cb6f49b81570dd452
Sylvie <Sylvie@users.noreply.github.com>
2019-01-12 11:02:21 +0100

    SMB-52: Test avec coordonnées cartesiennes et pondération gaussienne
    Ce test est basé sur les données "random sample", car l'autocorrélation
    spatiale avec la pondération gaussienne est bien trop longue à calculer
    pour les données bovines.

--- test/integration/sambada/spatialAutocorrelationResultsIntTests/Makefile.mk
+++ test/integration/sambada/spatialAutocorrelationResultsIntTests/Makefile.mk
@@ -1,13 +1,16 @@
 
 test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/spatialAutocorrelationResultsIntTests/spatialAutocorrelationCartesianBisquareIntTests.cpp \
-		test/integration/sambada/spatialAutocorrelationResultsIntTests/spatialAutocorrelationCartesianDistanceIntTests.cpp
+		test/integration/sambada/spatialAutocorrelationResultsIntTests/spatialAutocorrelationCartesianDistanceIntTests.cpp \
+		test/integration/sambada/spatialAutocorrelationResultsIntTests/spatialAutocorrelationCartesianGaussianIntTests.cpp
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/cattle-pop-env.csv \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/cattle-pop-mark.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/random-sample.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/param-cartesian-bisqure.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/param-cartesian-distance.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/param-random-sample-cartesian-gausian.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-bisquare-env.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-bisquare-env-pval.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-bisquare-mark.txt \
@@ -15,6 +18,9 @@ EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-distance-env.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-distance-env-pval.txt \
 		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-distance-mark.txt \
-		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-distance-mark-pval.txt
-
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-distance-mark-pval.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-gaussian-env.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-gaussian-env-pval.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-gaussian-mark.txt \
+		$(top_srcdir)/test/integration/sambada/spatialAutocorrelationResultsIntTests/expected-results-cartesian-gaussian-mark-pval.txt
 
