33d41cbf8cc85cbb5c569a9d1dcd7006938fd101
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-01-13 12:39:45 +0100

    SMB-52: Activation des tests d'autocorrélation spatiale de base (2)
    Correction des noms des fichiers à distribuer.

--- test/integration/sambada/basicSpatialAutocorrelationIntTests/Makefile.mk
+++ test/integration/sambada/basicSpatialAutocorrelationIntTests/Makefile.mk
@@ -5,8 +5,8 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/param-random-sample.txt \
 		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/random-sample.txt \
-		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/random-sample-AS-Env.txt \
-		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/random-sample-AS-Env-pVal.txt \
-		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/random-sample-AS-Mark.txt \
-		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/random-sample-AS-Mark-pVal.txt
+		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/expected-results-random-sample-AS-Env.txt \
+		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/expected-results-random-sample-AS-Env-pVal.txt \
+		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/expected-results-random-sample-AS-Mark.txt \
+		$(top_srcdir)/test/integration/sambada/basicSpatialAutocorrelationIntTests/expected-results-random-sample-AS-Mark-pVal.txt
 
