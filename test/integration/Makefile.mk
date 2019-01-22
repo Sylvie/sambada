9fc132a16e5681a831ee6e364701d37374853eb9
Sylvie <Sylvie@users.noreply.github.com>
2019-01-22 20:53:24 +0100

    SMB-54: Effacement des fichiers "*.dSYM" générés
    Ces fichier sont générés quand on active l'option de compilation -g.

--- test/integration/Makefile.mk
+++ test/integration/Makefile.mk
@@ -7,3 +7,4 @@ check-local-test-integration: check-local-test-integration-sambada
 
 ## Cleaning ##
 clean-local-test-integration: clean-local-test-integration-sambada clean-local-test-integration-utils
+	$(call clean_extra_generated_files, test/integration/)
