dc7ddf841c33b9c89018bfb5386b1a53d7e111a6
Sylvie <Sylvie@users.noreply.github.com>
2019-02-04 10:22:27 +0100

    SMB-66: Ajout de tests pour les histogrammes avec structure de pop
    Ces tests vérifient que les histogrammes comprenant les deux lignes
    supplémentaires pour le score G et le score Wald avec population sont
    corrects.
    
    Il y a un test pour le cas où les variables de pop sont placées avant
    les variables environnementales et un test pour le cas inverse.

---
+++ test/integration/sambada/storeyHistograms/storeyHistogramsWithPopulationStructureAsLastVariablesIntTests.cpp
@@ -0,0 +1,79 @@
+#include "catch.hpp"
+#include "SambadaStoreyHistogram.h"
+#include "SambadaIntegrationTestUtils.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+SCENARIO("Test that Storey's p-values histograms are correct with population structure as last variables",
+		"[storey-histograms-with-pop-structure-as-last-variables-int][storey-histograms-int]") {
+
+    INFO("Working folder: " + SambadaIntegrationTestUtils::runCommand("pwd"));
+
+    GIVEN("A program to run, its expected output file, and the file of expected results")
+    {
+        std::string program(SambadaIntegrationTestUtils::computePlatformSpecificProgramName("./binaries/sambada"));
+
+        std::string pathToOutputFolder("./test/integration/sambada/storeyHistograms/");
+        std::string fileNameOut0(pathToOutputFolder + "cattle-pop-mark-Out-0.txt");
+        std::string fileNameOut1(pathToOutputFolder + "cattle-pop-mark-Out-1.txt");
+        std::string fileNameOut2(pathToOutputFolder + "cattle-pop-mark-Out-2.txt");
+        std::string fileNameOut3(pathToOutputFolder + "cattle-pop-mark-Out-3.txt");
+	    std::string fileNameHistogram(pathToOutputFolder + "cattle-pop-mark-storey.txt");
+	    std::string fileNameLogs(pathToOutputFolder + "cattle-pop-mark-log.txt");
+
+        std::vector<std::string> outputFileNames({fileNameOut0, fileNameOut1, fileNameOut2, fileNameOut3, fileNameHistogram, fileNameLogs});
+
+        std::string pathToInputFolder(SambadaIntegrationTestUtils::getTopSourceDirectory() + "test/integration/sambada/storeyHistograms/");
+
+        std::string fileNameParam(pathToInputFolder + "param-with-pop-structure-as-last-variables.txt");
+        std::string fileNameEnv(pathToInputFolder + "cattle-pop-env-last.csv");
+        std::string fileNameMark(pathToInputFolder + "cattle-pop-mark.txt");
+
+	    std::string fileNameExpectedResults(pathToInputFolder + "expected-storey-histograms-with-pop-structure-as-last-variables.txt");
+
+	    int numberHistograms(14);
+
+        std::ifstream lecteurCorrige(fileNameExpectedResults.c_str());
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaStoreyHistogram expectedResults(SambadaIntegrationTestUtils::readStoreyHistogram(lecteurCorrige));
+        lecteurCorrige.close();
+	    expectedResults.verifieTailles(numberHistograms);
+
+        CHECK_FALSE(SambadaIntegrationTestUtils::doesAnyFileExist(outputFileNames));
+
+        WHEN("the program is run")
+        {
+            std::string output = SambadaIntegrationTestUtils::runCommand(program + " " + fileNameParam + " " + fileNameEnv + " " + fileNameMark);
+            //INFO(output);
+
+            THEN("the output file with the histograms is found")
+            {
+                std::ifstream lecteur(fileNameHistogram.c_str());
+
+                REQUIRE(lecteur.good());
+                REQUIRE(lecteur.is_open());
+
+                AND_WHEN("the file is read")
+                {
+                    SambadaStoreyHistogram results(
+                            SambadaIntegrationTestUtils::readStoreyHistogram(lecteur));
+
+                    THEN("the results match the expectation")
+                    {
+                        results.verifieTailles(numberHistograms);
+                        results.compare(expectedResults);
+                    }
+                }
+
+                lecteur.close();
+            }
+
+            SambadaIntegrationTestUtils::removeFiles(outputFileNames);
+        }
+    }
+}
+
