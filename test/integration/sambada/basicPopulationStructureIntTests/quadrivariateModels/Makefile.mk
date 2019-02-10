f0e45be25d1422557f2fcd4f24d8863f0c6cf598
Sylvie <Sylvie@users.noreply.github.com>
2019-02-10 16:07:17 +0100

    SMB-67: Ajout de tests en dimension 4 (avec LAST)
    Ces test comprennent 3 variables de population et une variable
    environnementale, placée dans la première colonne.

--- test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/Makefile.mk
+++ test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/Makefile.mk
@@ -1,6 +1,7 @@
 
 test_integration_SambadaIntegrationTests_SOURCES += \
-		test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/populationStructureAsFirstVariablesQuadrivariateModelsIntTests.cpp
+		test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/populationStructureAsFirstVariablesQuadrivariateModelsIntTests.cpp \
+		test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/populationStructureAsLastVariablesQuadrivariateModelsIntTests.cpp
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-dim-0.txt \
@@ -8,4 +9,8 @@ EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-3.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-4.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-2.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-4.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/param-quadri-pop-first.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/quadrivariateModels/param-quadri-pop-first.txt
