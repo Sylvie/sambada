c5f42907eb1621a3c6898fb1738838ed429d9267
Sylvie <Sylvie@users.noreply.github.com>
2019-02-23 11:38:15 +0100

    SMB-72: Ajout des résultats des modèles dans les tests univariés
    Seuls les histogrammes étaient vérifiés jusqu'à présent.

--- test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/Makefile.mk
+++ test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/Makefile.mk
@@ -3,5 +3,7 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/storeyHistogramsWithScoreThresholdDim1IntTests.cpp
 
 EXTRA_DIST += \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/expected-results-dim-0.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/expected-results-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/expected-storey-histograms-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/univariateModels/param-dim-1.txt
