b0871a7268e276eae07c84d145cf5e915c5965db
Sylvie <Sylvie@users.noreply.github.com>
2019-02-23 10:36:02 +0100

    SMB-72: Raccourcissement des noms de deux tests
    Voyons si ça résout le problème sur Windows.

--- test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/Makefile.mk
+++ test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/Makefile.mk
@@ -1,7 +1,7 @@
 
 test_integration_SambadaIntegrationTests_SOURCES += \
-		test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistogramsWithScoreThresholdAndPopulationStructureAsFirstVariablesDim4IntTests.cpp \
-		test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistogramsWithScoreThresholdAndPopulationStructureAsLastVariablesDim4IntTests.cpp
+		test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistWithScoreThresholdAndPopStructAsFirstVarsDim4IntTests.cpp \
+		test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistWithScoreThresholdAndPopStructAsLastVarsDim4IntTests.cpp
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-storey-histograms-with-pop-structure-as-first-variables-dim-4.txt \
