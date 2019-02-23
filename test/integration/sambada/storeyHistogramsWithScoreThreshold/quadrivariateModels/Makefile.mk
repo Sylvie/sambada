035808c1e0f6f4b7a9536db3f018e2fbd0b1c381
Sylvie <Sylvie@users.noreply.github.com>
2019-02-23 15:07:52 +0100

    SMB-72: Ajout des résultats des modèles dans les tests quadrivariés
    Seuls les histogrammes étaient vérifiés jusqu'à présent.

--- test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/Makefile.mk
+++ test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/Makefile.mk
@@ -4,6 +4,14 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistWithScoreThresholdAndPopStructAsLastVarsDim4IntTests.cpp
 
 EXTRA_DIST += \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-dim-0.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-dim-1.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-first-dim-2.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-first-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-first-dim-4.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-last-dim-2.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-last-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-results-quadri-pop-last-dim-4.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-storey-histograms-with-pop-structure-as-first-variables-dim-4.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/expected-storey-histograms-with-pop-structure-as-last-variables-dim-4.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/param-with-pop-structure-as-first-variables-dim-4.txt \
--- test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistWithScoreThresholdAndPopStructAsLastVarsDim4IntTests.cpp
+++ test/integration/sambada/storeyHistogramsWithScoreThreshold/quadrivariateModels/storeyHistWithScoreThresholdAndPopStructAsLastVarsDim4IntTests.cpp
@@ -36,6 +36,11 @@ SCENARIO("Test that Storey's p-values histograms are correct with score threshol
         std::string fileNameMark(pathToInputFolder + "../cattle-pop-mark.txt");
 
 	    std::string fileNameExpectedResults(pathToInputFolder + "expected-storey-histograms-with-pop-structure-as-last-variables-dim-4.txt");
+        std::string fileNameExpectedResultsDim0(pathToInputFolder + "expected-results-quadri-dim-0.txt");
+        std::string fileNameExpectedResultsDim1(pathToInputFolder + "expected-results-quadri-dim-1.txt");
+        std::string fileNameExpectedResultsDim2(pathToInputFolder + "expected-results-quadri-pop-last-dim-2.txt");
+        std::string fileNameExpectedResultsDim3(pathToInputFolder + "expected-results-quadri-pop-last-dim-3.txt");
+        std::string fileNameExpectedResultsDim4(pathToInputFolder + "expected-results-quadri-pop-last-dim-4.txt");
 
 	    int numberHistograms(18);
 
@@ -46,33 +51,140 @@ SCENARIO("Test that Storey's p-values histograms are correct with score threshol
         lecteurCorrige.close();
 	    expectedResults.verifieTailles(numberHistograms);
 
+        lecteurCorrige.open(fileNameExpectedResultsDim0.c_str());
+        INFO("Reading expected results dim 0");
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedNullResults(
+                SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 0));
+        lecteurCorrige.close();
+        expectedNullResults.verifieTailles(true, 0, 30);
+
+        lecteurCorrige.open(fileNameExpectedResultsDim1.c_str());
+        INFO("Reading expected results dim 1");
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedResultsDim1(
+                SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 1));
+        lecteurCorrige.close();
+        expectedResultsDim1.verifieTailles(true, 1, 240);
+
+        lecteurCorrige.open(fileNameExpectedResultsDim2.c_str());
+        INFO("Reading expected results dim 2");
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedResultsDim2(
+                SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 2));
+        lecteurCorrige.close();
+        expectedResultsDim2.verifieTailles(true, 2, 840);
+
+        lecteurCorrige.open(fileNameExpectedResultsDim3.c_str());
+        INFO("Reading expected results dim 3");
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedResultsDim3(
+                SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 3));
+        lecteurCorrige.close();
+        expectedResultsDim3.verifieTailles(true, 3, 30);
+
+        lecteurCorrige.open(fileNameExpectedResultsDim4.c_str());
+        INFO("Reading expected results dim 4");
+        REQUIRE(lecteurCorrige.good());
+        REQUIRE(lecteurCorrige.is_open());
+        SambadaRegressionResults expectedResultsDim4(
+                SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 4));
+        lecteurCorrige.close();
+        expectedResultsDim4.verifieTailles(true, 4, 150, true);
+
         CHECK_FALSE(SambadaIntegrationTestUtils::doesAnyFileExist(outputFileNames));
 
-        WHEN("the program is run")
+        WHEN("Sambada is run using the last variables as population structure")
         {
             std::string output = SambadaIntegrationTestUtils::runCommand(program + " " + fileNameParam + " " + fileNameEnv + " " + fileNameMark);
             INFO(output);
 
-            THEN("the output file with the histograms is found")
+            THEN("the output files are found")
             {
+                INFO("Reading histograms");
                 std::ifstream lecteur(fileNameHistogram.c_str());
-
                 REQUIRE(lecteur.good());
                 REQUIRE(lecteur.is_open());
 
-                AND_WHEN("the file is read")
+                INFO("Reading results dim 0");
+                std::ifstream lecteurOut0(fileNameOut0.c_str());
+                REQUIRE(lecteurOut0.good());
+                REQUIRE(lecteurOut0.is_open());
+
+                INFO("Reading results dim 1");
+                std::ifstream lecteurOut1(fileNameOut1.c_str());
+                REQUIRE(lecteurOut1.good());
+                REQUIRE(lecteurOut1.is_open());
+
+                INFO("Reading results dim 2");
+                std::ifstream lecteurOut2(fileNameOut2.c_str());
+                REQUIRE(lecteurOut2.good());
+                REQUIRE(lecteurOut2.is_open());
+
+                INFO("Reading results dim 3");
+                std::ifstream lecteurOut3(fileNameOut3.c_str());
+                REQUIRE(lecteurOut3.good());
+                REQUIRE(lecteurOut3.is_open());
+
+                INFO("Reading results dim 4");
+                std::ifstream lecteurOut4(fileNameOut4.c_str());
+                REQUIRE(lecteurOut4.good());
+                REQUIRE(lecteurOut4.is_open());
+
+                AND_WHEN("the output files are read")
                 {
                     SambadaStoreyHistogram results(
                             SambadaIntegrationTestUtils::readStoreyHistogram(lecteur));
+                    SambadaRegressionResults resultsDim0(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut0, true, 0));
+                    SambadaRegressionResults resultsDim1(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut1, true, 1));
+                    SambadaRegressionResults resultsDim2(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut2, true, 2));
+                    SambadaRegressionResults resultsDim3(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut3, true, 3));
+                    SambadaRegressionResults resultsDim4(
+                            SambadaIntegrationTestUtils::readRegressionResults(lecteurOut4, true, 4));
 
                     THEN("the results match the expectation")
                     {
+                        INFO("Verifying histograms");
                         results.verifieTailles(numberHistograms);
                         results.compare(expectedResults);
+
+                        INFO("Verifying results dim 0");
+                        resultsDim0.verifieTailles(true, 0, 30);
+                        resultsDim0.compare(expectedNullResults);
+
+                        INFO("Verifying results dim 1");
+                        resultsDim1.verifieTailles(true, 1, 240);
+                        resultsDim1.compare(expectedResultsDim1);
+
+                        INFO("Verifying results dim 2");
+                        resultsDim2.verifieTailles(true, 2, 840);
+                        resultsDim2.compare(expectedResultsDim2, 0.001);
+
+                        INFO("Verifying results dim 3");
+                        resultsDim3.verifieTailles(true, 3, 30);
+                        resultsDim3.compare(expectedResultsDim3);
+
+                        INFO("Verifying results dim 4");
+                        resultsDim4.verifieTailles(true, 4, 150, true);
+                        resultsDim4.compare(expectedResultsDim4, 0.001);
+
                     }
                 }
 
                 lecteur.close();
+                lecteurOut0.close();
+                lecteurOut1.close();
+                lecteurOut2.close();
+                lecteurOut3.close();
+                lecteurOut4.close();
             }
 
             SambadaIntegrationTestUtils::removeFiles(outputFileNames);
