a3896887c6015c0e88c5a740ea8800b09feeaf0e
Sylvie <Sylvie@users.noreply.github.com>
2019-03-03 16:20:08 +0100

    SMB-72: Modification des tests quadrivariés avec seuil et avec SAVETYPE REAL (2)
    Correction du Makefile.am.

--- test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/Makefile.mk
+++ test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/Makefile.mk
@@ -5,10 +5,11 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-dim-0.txt \
-		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-dim-1.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-3.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-first-dim-4.txt \
+		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-3.txt \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsScoreThresholdIntTests/quadrivariateModels/expected-results-quadri-pop-last-dim-4.txt \
