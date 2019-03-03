84aa4bd6776ffb75ea47df8fcb63c801cfaff206
Sylvie <Sylvie@users.noreply.github.com>
2019-03-03 19:55:18 +0100

    SMB-72: Ajout de test trivariés sans pop, sans seuil et avec SAVETYPE REAL (2)
    Correction du Makefile.am.

--- test/integration/sambada/storeyHistogramsIntTests/trivariateModelsReal/Makefile.mk
+++ test/integration/sambada/storeyHistogramsIntTests/trivariateModelsReal/Makefile.mk
@@ -1,6 +1,6 @@
 
 test_integration_SambadaIntegrationTests_SOURCES += \
-		test/integration/sambada/storeyHistogramsIntTests/trivariateModelsReal/storeyHistogramsDim3IntTests.cpp
+		test/integration/sambada/storeyHistogramsIntTests/trivariateModelsReal/storeyHistogramsSavetypeRealDim3IntTests.cpp
 
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/storeyHistogramsIntTests/trivariateModelsReal/expected-results-no-pop-dim-0.txt \
