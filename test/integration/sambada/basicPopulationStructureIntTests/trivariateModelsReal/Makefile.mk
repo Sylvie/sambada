ffff946268a9e14a82cde3d573596df28c1e9101
Sylvie <Sylvie@users.noreply.github.com>
2019-03-06 22:12:30 +0100

    SMB-72: Ajout de test trivariés sans STOREY et avec SAVETYPE REAL (2)
    Correction du Makefile.am

--- test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/Makefile.mk
+++ test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/Makefile.mk
@@ -9,8 +9,10 @@ EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-no-pop-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-no-pop-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-no-pop-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-pop-first-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-pop-first-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-pop-first-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-pop-last-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/expected-results-pop-last-dim-3.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/param-no-pop.txt \
 		$(top_srcdir)/test/integration/sambada/basicPopulationStructureIntTests/trivariateModelsReal/param-pop-first.txt \
