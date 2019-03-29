228dc8121122c209ab891ebc4f53186a6fbf4212
Sylvie <Sylvie@users.noreply.github.com>
2019-03-29 14:22:53 +0100

    SMB-41: Test de la fusion de fichiers avec tri selon le score G
    Le test s'appelle mergeResultFilesWithGScoreSortIntTests.cpp.

---
+++ test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/Makefile.mk
@@ -0,0 +1,21 @@
+
+test_integration_SupervisionIntegrationTests_SOURCES += \
+		test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mergeResultFilesWithGScoreSortIntTests.cpp
+
+EXTRA_DIST += \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/expected-results-dim-0.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/expected-results-G-sort-dim-1.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/expected-results-G-sort-dim-2.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/expected-results-G-sort-dim-3.txt \
+        $(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-0-0-Out-0.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-0-0-Out-1.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-0-0-Out-2.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-0-0-Out-3.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-1-12-Out-0.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-1-12-Out-1.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-1-12-Out-2.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-1-12-Out-3.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-2-24-Out-0.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-2-24-Out-1.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-2-24-Out-2.txt \
+		$(top_srcdir)/test/integration/supervision/mergeResultFilesWithGScoreSortIntTests/mol-data-mark-2-24-Out-3.txt
