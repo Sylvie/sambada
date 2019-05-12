d03cfc15ddd5688c22c8c3db7c97a79863251adf
Sylvie <Sylvie@users.noreply.github.com>
2019-05-12 17:28:21 +0200

    SMB-80: Problème de time-out sur Travis (3)
    ... et effaçage de la commande dans le script secondaire.

--- scripts-ci/travis-build.sh
+++ scripts-ci/travis-build.sh
@@ -15,14 +15,14 @@ else
     build-wrapper-linux-x86-64 --out-dir bw-outputs make
     ls bw-outputs
 fi
-travis_wait make check
+make check
 cp test-suite.log ../test-suite-results.log
 if [ "${SAMBADA_BUILD_TYPE}" = "PACKAGE" ]; then
     binaries/sambada ../Data/Example_parameters_input_matrix.txt ../Data/input_matrix.csv
     make binary-archive
     ls -R
     make clean
-    travis_wait make distcheck sambadahostsystemname=${SAMBADA_OS_NAME}
+    make distcheck sambadahostsystemname=${SAMBADA_OS_NAME}
 else
     pwd
     source ${TRAVIS_BUILD_DIR}/scripts-ci/travis-gcov.sh
