b88621a9dcd593f3a34cee67189bc70763ae4ac8
Sylvie <Sylvie@users.noreply.github.com>
2019-05-12 16:49:42 +0200

    SMB-80: Problème de time-out sur Travis
    Les tests prennent plus de 10 min, ce qui déclenche un time-out.
    
    Les commande "make check" et "make distcheck" sont préfixées avec
    "travis_wait".
    
    Voyons si ça résout le problème.

--- scripts-ci/travis-build.sh
+++ scripts-ci/travis-build.sh
@@ -15,14 +15,14 @@ else
     build-wrapper-linux-x86-64 --out-dir bw-outputs make
     ls bw-outputs
 fi
-make check
+travis_wait make check
 cp test-suite.log ../test-suite-results.log
 if [ "${SAMBADA_BUILD_TYPE}" = "PACKAGE" ]; then
     binaries/sambada ../Data/Example_parameters_input_matrix.txt ../Data/input_matrix.csv
     make binary-archive
     ls -R
     make clean
-    make distcheck sambadahostsystemname=${SAMBADA_OS_NAME}
+    travis_wait make distcheck sambadahostsystemname=${SAMBADA_OS_NAME}
 else
     pwd
     source ${TRAVIS_BUILD_DIR}/scripts-ci/travis-gcov.sh
