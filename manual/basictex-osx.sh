2e6ba54e8c928c468f4298ef706a14abcd7c7ae1
Sylvie <Sylvie@users.noreply.github.com>
2019-05-12 14:45:56 +0200

    SMB-80: Mise à jour de BasicTeX pour macOS
    La version 2019 est disponible.

--- manual/basictex-osx.sh
+++ manual/basictex-osx.sh
@@ -13,7 +13,7 @@ export PATH=$PATH:"/usr/local/texlive/2018basic/bin/x86_64-darwin"
 if ! command -v latexmk > /dev/null; then
 
 # Obtain BasicTeX
-wget http://mirror.ox.ac.uk/sites/ctan.org/systems/mac/mactex/mactex-basictex-20180417.pkg -O BasicTeX.pkg
+wget http://mirror.ox.ac.uk/sites/ctan.org/systems/mac/mactex/mactex-basictex-20190410.pkg -O BasicTeX.pkg
 
 # Install a minimal system
 sudo installer -pkg BasicTeX.pkg -target /
