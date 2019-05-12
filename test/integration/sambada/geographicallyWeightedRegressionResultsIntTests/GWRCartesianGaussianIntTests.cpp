d445e102d00c5a26fddfbb9705aef3757dc61fc7
Sylvie <Sylvie@users.noreply.github.com>
2019-05-12 16:44:51 +0200

    SMB-80: Ajout de tests GWR pour le cas cartesian / gaussian
    Ces tests calculent des modèles bivariés comprennant des GWR,
    pour le cas des coordonnées cartésiennes avec pondération "gaussienne".
    
    Ces tests utilisent les données aléatoires, car les calculs sont trop
    long avec les données des bovins ougandais.

---
+++ test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianGaussianIntTests.cpp
@@ -0,0 +1,128 @@
+#include "catch.hpp"
+#include "SambadaRegressionResults.hpp"
+#include "SambadaIntegrationTestUtils.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+SCENARIO("Test that geographically weighted regression is computed correctly for cartesian coordinates and \"gaussian\" weighting ",
+         "[gwr-cartesian-gaussian-int][gwr-int]")
+{
+
+	INFO("Working folder: " + SambadaIntegrationTestUtils::runCommand("pwd"));
+
+	GIVEN("Some input files with info on population structure")
+	{
+		std::string program(SambadaIntegrationTestUtils::computePlatformSpecificProgramName("./binaries/sambada"));
+
+		std::string pathToTestFolder("test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/");
+
+		std::string pathToOutputFolder("./" + pathToTestFolder);
+
+		std::string fileNameOut0(pathToOutputFolder + "random-sample-Out-0.txt");
+		std::string fileNameOut1(pathToOutputFolder + "random-sample-Out-1.txt");
+		std::string fileNameOut2(pathToOutputFolder + "random-sample-Out-2.txt");
+		std::string fileNameLogs(pathToOutputFolder + "random-sample-log.txt");
+
+		std::vector<std::string> outputFileNames({fileNameOut0, fileNameOut1, fileNameOut2, fileNameLogs});
+
+		std::string pathToInputFolder(
+				SambadaIntegrationTestUtils::getTopSourceDirectory() + pathToTestFolder);
+
+		std::string fileNameParams(pathToInputFolder + "param-cartesian-gaussian.txt");
+		std::string fileNameData(pathToInputFolder + "random-sample.txt");
+
+		std::string fileNameExpectedResultsDim0(pathToInputFolder + "expected-results-random-sample-dim-0.txt");
+		std::string fileNameExpectedResultsDim1(pathToInputFolder + "expected-results-cartesian-gaussian-dim-1.txt");
+		std::string fileNameExpectedResultsDim2(pathToInputFolder + "expected-results-cartesian-gaussian-dim-2.txt");
+
+		std::ifstream lecteurCorrige(fileNameExpectedResultsDim0.c_str());
+		INFO("Reading expected results dim 0");
+		REQUIRE(lecteurCorrige.good());
+		REQUIRE(lecteurCorrige.is_open());
+		SambadaRegressionResults expectedNullResults(
+				SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 0));
+		lecteurCorrige.close();
+		expectedNullResults.verifieTailles(true, 0, 1, false, true);
+
+		lecteurCorrige.open(fileNameExpectedResultsDim1.c_str());
+		INFO("Reading expected results dim 1");
+		REQUIRE(lecteurCorrige.good());
+		REQUIRE(lecteurCorrige.is_open());
+		SambadaRegressionResults expectedResultsDim1(
+				SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 1));
+		lecteurCorrige.close();
+		expectedResultsDim1.verifieTailles(true, 1, 2, false, true);
+
+		lecteurCorrige.open(fileNameExpectedResultsDim2.c_str());
+		INFO("Reading expected results dim 2");
+		REQUIRE(lecteurCorrige.good());
+		REQUIRE(lecteurCorrige.is_open());
+		SambadaRegressionResults expectedResultsDim2(
+				SambadaIntegrationTestUtils::readRegressionResults(lecteurCorrige, true, 2));
+		lecteurCorrige.close();
+		expectedResultsDim2.verifieTailles(true, 2, 1, false, true);
+
+		WHEN("Sambada is run using the first variables as population structure")
+		{
+			CHECK_FALSE(SambadaIntegrationTestUtils::doesAnyFileExist(outputFileNames));
+
+			std::string output = SambadaIntegrationTestUtils::runCommand(
+					program + " " + fileNameParams + " " + fileNameData);
+			//INFO(output);
+
+			THEN("the output files are found")
+			{
+				INFO("Reading results dim 0");
+				std::ifstream lecteurOut0(fileNameOut0.c_str());
+				REQUIRE(lecteurOut0.good());
+				REQUIRE(lecteurOut0.is_open());
+
+				INFO("Reading results dim 1");
+				std::ifstream lecteurOut1(fileNameOut1.c_str());
+				REQUIRE(lecteurOut1.good());
+				REQUIRE(lecteurOut1.is_open());
+
+				INFO("Reading results dim 2");
+				std::ifstream lecteurOut2(fileNameOut2.c_str());
+				REQUIRE(lecteurOut2.good());
+				REQUIRE(lecteurOut2.is_open());
+
+
+				AND_WHEN("the output files are read")
+				{
+					SambadaRegressionResults resultsDim0(
+							SambadaIntegrationTestUtils::readRegressionResults(lecteurOut0, true, 0));
+					SambadaRegressionResults resultsDim1(
+							SambadaIntegrationTestUtils::readRegressionResults(lecteurOut1, true, 1));
+					SambadaRegressionResults resultsDim2(
+							SambadaIntegrationTestUtils::readRegressionResults(lecteurOut2, true, 2));
+
+					THEN("the results match the expectations")
+					{
+						INFO("Verifying results dim 0");
+						resultsDim0.verifieTailles(true, 0, 1, false, true);
+						resultsDim0.compare(expectedNullResults);
+
+						INFO("Verifying results dim 1");
+						resultsDim1.verifieTailles(true, 1, 2, false, true);
+						resultsDim1.compare(expectedResultsDim1, 0.01);
+
+						INFO("Verifying results dim 2");
+						resultsDim2.verifieTailles(true, 2, 1, false, true);
+						resultsDim2.compare(expectedResultsDim2, 0.01);
+					}
+				}
+
+				lecteurOut0.close();
+				lecteurOut1.close();
+				lecteurOut2.close();
+			}
+		}
+
+		SambadaIntegrationTestUtils::removeFiles(outputFileNames);
+	}
+}
+
--- test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/Makefile.mk
+++ test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/Makefile.mk
@@ -2,10 +2,10 @@
 test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianBisquareIntTests.cpp \
 		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianDistanceIntTests.cpp \
+		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianGaussianIntTests.cpp \
 		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRSphericalBisquareIntTests.cpp \
 		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRSphericalDistanceIntTests.cpp
 
-#		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianGaussianIntTests.cpp \
 #		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRCartesianNearestIntTests.cpp \
 #		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRSphericalDistanceIntTests.cpp \
 #		test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/GWRSphericalGaussianIntTests.cpp \
@@ -15,8 +15,10 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/cattle-pop-env.csv \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/cattle-pop-mark.txt \
+		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/random-sample.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-cartesian-bisquare.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-cartesian-distance.txt \
+		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-cartesian-gaussian.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-spherical-bisquare.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-spherical-distance.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-dim-0.txt \
@@ -24,30 +26,21 @@ EXTRA_DIST += \
    		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-bisquare-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-distance-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-distance-dim-2.txt \
+		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-dim-1.txt \
+		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-bisquare-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-bisquare-dim-2.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-dim-2.txt
 
 
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/random-sample.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-cartesian-nearest.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-spherical-distance.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-spherical-nearest.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-random-sample-cartesian-gaussian.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/param-random-sample-spherical-gaussian.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-env.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-env-pval.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-mark.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-gaussian-mark-pval.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-nearest-env.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-nearest-env-pval.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-nearest-mark.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-cartesian-nearest-mark-pval.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-env.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-env-pval.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-mark.txt \
-#		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-distance-mark-pval.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-gaussian-env.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-gaussian-env-pval.txt \
 #		$(top_srcdir)/test/integration/sambada/geographicallyWeightedRegressionResultsIntTests/expected-results-spherical-gaussian-mark.txt \
