2cd8be25aac00b40f8937da72ee995746072032f
Sylvie <Sylvie@users.noreply.github.com>
2019-05-18 22:07:56 +0200

    SMB-82: Modification du Makefile.am des tests
    Le Makefile.am des tests inclut maintenant la bibliothèque intermédiaire
    libvariables.a.
    
    Cela réduit le nombre d'objets compilés et ça devrait permettre de
    calculer la couverture du code pour les nouveaux fichiers.

--- test/unit/sambada/variables/Makefile.mk
+++ test/unit/sambada/variables/Makefile.mk
@@ -1,16 +1,12 @@
 ## Adding unit tests for variables
 
 test_unit_SambadaUnitTests_SOURCES +=  \
-	test/unit/sambada/variables/FamilleVariablesFactoryTests.cpp \
-	src/variables/CombinaisonVariables.cpp \
-    src/variables/CombinaisonVariables.hpp \
-    src/variables/FamilleVariablesFactory.cpp \
-    src/variables/FamilleVariablesFactory.hpp \
-    src/variables/TypesCommuns.hpp
+	test/unit/sambada/variables/FamilleVariablesFactoryTests.cpp
 
 test_unit_SambadaUnitTests_CPPFLAGS += -I $(top_srcdir)/ext/scythestat-1.0.3/scythestat \
 	-I $(top_srcdir)/ext/scythestat-1.0.3/scythestat/rng
 test_unit_SambadaUnitTests_CXXFLAGS = -D SCYTHE_COMPILE_DIRECT
+test_unit_SambadaUnitTests_LDADD += libvariables.a
 
 ## Cleaning ##
 clean-local-test-unit-sambada-variables:
