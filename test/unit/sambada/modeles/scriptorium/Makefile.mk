737b6af8a9c22de6ac04fca2a821abebe5506ede
Sylvie <Sylvie@users.noreply.github.com>
2019-11-13 18:40:46 +0100

    SMB-87: Déplacement de deux tests (2)
    Mise à jour des builds.

--- test/unit/sambada/modeles/scriptorium/Makefile.mk
+++ test/unit/sambada/modeles/scriptorium/Makefile.mk
@@ -1,12 +1,11 @@
 ## Adding unit tests for variables
 
-test_unit_SambadaUnitTests_SOURCES +=  \
-	test/unit/sambada/modeles/scriptorium/FlotSortieChaineFactoryTests.cpp \
-	test/unit/sambada/modeles/scriptorium/ScribeTests.cpp
+test_unit_SambadaUnitTests_SOURCES +=
 
 include test/unit/sambada/modeles/scriptorium/lecteur/Makefile.mk
+include test/unit/sambada/modeles/scriptorium/scribe/Makefile.mk
 
 ## Cleaning ##
-clean-local-test-unit-sambada-modeles-scriptorium: clean-local-test-unit-sambada-modeles-scriptorium-lecteur
+clean-local-test-unit-sambada-modeles-scriptorium: clean-local-test-unit-sambada-modeles-scriptorium-lecteur clean-local-test-unit-sambada-modeles-scriptorium-scribe
 	$(call clean_extra_generated_files, test/unit/sambada/modeles/scriptorium)
 
