c17044d37ab46da3689120327b145e6cfe47a0b9
Sylvie <Sylvie@users.noreply.github.com>
2020-05-07 21:27:28 +0200

    SMB-95: Ajout de la classe RegresseurLogistiqueTests
    Le test existant échoue, car la méthode calculeRegression() n'est pas
    encore implémentée par le RegresseurLogistique.

---
+++ test/unit/sambada/modeles/calibration/Makefile.mk
@@ -0,0 +1,9 @@
+## Adding unit tests for variables
+
+test_unit_SambadaUnitTests_SOURCES += \
+	RegresseurLogistiqueTests.cpp
+
+## Cleaning ##
+clean-local-test-unit-sambada-modeles-calibration:
+	$(call clean_extra_generated_files, test/unit/sambada/modeles/calibration)
+
