1538e1a7a1e62daec99ce7c8ed1c5e9e291d895e
Sylvie <Sylvie@users.noreply.github.com>
2020-08-03 21:20:04 +0200

    SMB-109: Ajout d'un test en dim 3 pour SAVETYPE REAL
    Cela permettra de tester la sauvegarde à la fin des calculs avec les
    tests "principaux".

--- test/integration/sambada/monomorphicModelsRegressionResultsIntTests/Makefile.mk
+++ test/integration/sambada/monomorphicModelsRegressionResultsIntTests/Makefile.mk
@@ -3,7 +3,8 @@ test_integration_SambadaIntegrationTests_SOURCES += \
 		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim0IntTests.cpp \
 		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim1IntTests.cpp \
 		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim2IntTests.cpp \
-		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim3IntTests.cpp
+		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim3IntTests.cpp \
+		test/integration/sambada/monomorphicModelsRegressionResultsIntTests/regressionResultsDim3IntTests-saveTypeReal.cpp
 
 EXTRA_DIST += \
  		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/choice-env-cattle.csv \
@@ -15,4 +16,5 @@ EXTRA_DIST += \
 		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-0.txt \
 		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-1.txt \
 		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-2.txt \
-		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-3.txt
+		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-3.txt \
+		$(top_srcdir)/test/integration/sambada/monomorphicModelsRegressionResultsIntTests/param-dim-3-savetype-real.txt
