f464c00418d88cc3ee270c1072b0fbcb53418588
Sylvie <Sylvie@users.noreply.github.com>
2022-03-18 20:37:57 +0100

    SMB-122: Téléversement des artefacts avec un script (1)
    Essayons de réduire la redondance.

---
+++ .github/workflows/scripts/upload-artefact.js
@@ -0,0 +1,15 @@
+module.exports = async ({github, releaseId, fullpathToArtefact}) => {
+    console.log(`Release ID: ${releaseId}`);
+    const filename = fullpathToArtefact.split(['/','\\']).pop();
+    console.log(`Filename: ${filename}`);
+    const fs = require('fs').promises;
+    const response = await github.rest.repos.uploadReleaseAsset({
+        owner: 'Sylvie',
+        repo: 'Sambada',
+        release_id: releaseId,
+        name: filename,
+        data: await fs.readFile(fullpath)
+    });
+    console.log(`Response: ${JSON.stringify(response, null, "    ")}`);
+    return response;
+}
