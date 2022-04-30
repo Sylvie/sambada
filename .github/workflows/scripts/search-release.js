58d97453f7aca8745f2c4f7dd4386e843ed2c78a
Sylvie Stucki <Sylvie@users.noreply.github.com>
2022-04-30 18:32:47 +0200

    SMB-122: Recherche de la publication avec un script (5)
    Récupérons le numéro de publication

--- .github/workflows/scripts/search-release.js
+++ .github/workflows/scripts/search-release.js
@@ -29,6 +29,7 @@ module.exports = async ({github, context, core, tagName}) => {
         if (result["repository"]["release"] != null) {
             console.log("Found a release");
             core.setOutput("status", "found")
+            core.setOutput("result", result)
         }
         else
         {
