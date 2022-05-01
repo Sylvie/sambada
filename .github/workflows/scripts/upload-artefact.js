388773a51a997a8edd0336703b0391c5ecb82698
Sylvie <Sylvie@users.noreply.github.com>
2022-05-01 21:21:56 +0200

    SMB-122: Toilettage du processus (5)
    Mise au propre des téléverages d'artefacts

--- .github/workflows/scripts/upload-artefact.js
+++ .github/workflows/scripts/upload-artefact.js
@@ -1,15 +1,19 @@
 module.exports = async ({github, releaseId, fullpathToArtefact}) => {
-    console.log(`Release ID: ${releaseId}`);
-    const filename = fullpathToArtefact.split(/[\\\/]/).pop();
-    console.log(`Filename: ${filename}`);
-    const fs = require('fs').promises;
-    const response = await github.rest.repos.uploadReleaseAsset({
-        owner: 'Sylvie',
-        repo: 'Sambada',
-        release_id: releaseId,
-        name: filename,
-        data: await fs.readFile(fullpathToArtefact)
-    });
-    console.log(`Response: ${JSON.stringify(response, null, "    ")}`);
-    return response;
+    try {
+        console.log(`Release ID: ${releaseId}`);
+        const filename = fullpathToArtefact.split(/[\\\/]/).pop();
+        console.log(`Filename: ${filename}`);
+        const fs = require('fs').promises;
+        const response = await github.rest.repos.uploadReleaseAsset({
+            owner: 'Sylvie',
+            repo: 'Sambada',
+            release_id: releaseId,
+            name: filename,
+            data: await fs.readFile(fullpathToArtefact)
+        });
+        console.log(`Response: ${JSON.stringify(response, null, "    ")}`);
+        return response;
+    } catch (error) {
+        console.log(error.message)
+    }
 }
